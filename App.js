984c080d62381cd83a4f323c10c005d28ee8f5f3
Aman Rawat <148249738+AmanRawat94@users.noreply.github.com>
2023-11-26 18:55:30 +0530

    sixth

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./component/Navbar";
 import TextForm from "./component/TextForm";
+import About from "./component/About";
 
 function App() {
   return (
@@ -10,7 +11,11 @@ function App() {
 
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze" />
+        {/* <About/> */}
+
       </div>
+
+      <About/>
     </>
   );
 }
